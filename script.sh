159b722869bb65fb1f5462569f8c3d191c940986
AyoSal <ayosalawu@gmail.com>
2020-04-13 00:43:03 -0400

    new update - line 7

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ echo "This is an example of a script"
 echo "This is running ina build step"
 echo "Updating the text..This script is from github"
 echo "New line added"
+echo "New line"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUM"
