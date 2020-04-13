420ed3f545eadd5d003ab9292bd16449b48af7e1
AyoSal <ayosalawu@gmail.com>
2020-04-13 00:33:57 -0400

    updating

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 echo "This is an example of a script"
 echo "This is running ina build step"
-echo "This script is from github"
+echo "Updating the text..This script is from github"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUM"
